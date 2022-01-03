246a423172e83942e47fedff36ef14f9e98e4a7a
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-01-04 00:34:25 +0530

    Add files via upload

---
+++ IF.java
@@ -0,0 +1,9 @@
+public class IF{
+    public static void main(String[] args){
+        int a =20;
+        int b =20;
+        if(a==b){
+            System.out.println("both are same");
+        }
+    }
+}
