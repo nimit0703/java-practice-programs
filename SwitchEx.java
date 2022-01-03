03a435688c2b7e6663a552c3d248376d824f4c8c
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-01-04 00:55:43 +0530

    Add files via upload

---
+++ SwitchEx.java
@@ -0,0 +1,22 @@
+public class SwitchEx {
+    public static void main(String[] args){
+        String a = "banana";
+
+        switch("banananana"){
+            case "apple":
+                System.out.println("apple");
+                break;
+
+            case "mango":
+                System.out.println("mango");
+                break;
+            case "banana": 
+                System.out.println("banana");
+                break;
+            default:
+            System.out.println("it's not  a fruiet");
+
+        }
+    }
+    
+}
