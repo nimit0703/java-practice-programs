d52f1e636ba3e32866bcc391a841d5b00b8dc497
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-01-04 13:50:11 +0530

    Add files via upload

---
+++ ReflectivePyramid.java
@@ -0,0 +1,23 @@
+public class ReflectivePyramid { 
+    public static void main(String[] args){
+        int term = 6;
+        for(int i =1;i<=term;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        term-=1;
+        for(int i =1;i<=term;i++){
+            for(int j=term;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    
+}
+    
+
