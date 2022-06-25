a82df5832b0c8a76ea4d51ebb620282b0f1d7cde
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-06-25 23:13:44 +0530

    Create If.java

---
+++ Basics of java/If.java
@@ -0,0 +1,10 @@
+public class IF{
+    public static void main(String[] args){
+        int a =20;
+        int b =20;
+        if(a==b){
+            System.out.println("both are same");
+        }
+    }
+}
+© 2022 GitHub, Inc.
