38294b6a7e715a64940690169a8864c57e570a3b
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-06-25 23:14:24 +0530

    Create IfElse.java

---
+++ Basics of java/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args){
+        int a=10;
+        if(a==15){
+            System.out.println("a==15");
+        }
+        else{
+            System.out.println("a !=15");
+        }
+        }
+    
+}
