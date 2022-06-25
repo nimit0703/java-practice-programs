190fa533771aa9356f518d815c50048e9a6bf182
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-06-25 23:14:42 +0530

    Delete IfElseTernary.java

--- IfElseTernary.java
+++
@@ -1,9 +0,0 @@
-public class IfElseTernary {
-    public static void main(String[] args){
-        int year = 2001;
-        String output =(( year % 4 == 0) && (year % 100 !=0))?"leap year":"non leap year";
-        System.out.println(output);
-
-    }
-    
-}
