4181854892c579f4ee97501dfdc82711e9aa05d6
Nimit Trevadiya <68644815+nimit0703@users.noreply.github.com>
2022-06-25 23:13:25 +0530

    Delete IF.java

--- IF.java
+++
@@ -1,9 +0,0 @@
-public class IF{
-    public static void main(String[] args){
-        int a =20;
-        int b =20;
-        if(a==b){
-            System.out.println("both are same");
-        }
-    }
-}
